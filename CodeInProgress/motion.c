2188369ca9a544c56eb1cc3b46692ddceb1a095e
Florian <prods-cantos0w@icloud.com>
2023-06-13 13:14:35 +0200

    Add basic drive functions.

---
+++ CodeInProgress/motion.c
@@ -0,0 +1,40 @@
+#include "motion.h"
+#include "types.h"
+
+#define LEFT_MOTOR Port_B
+#define RIGHT_MOTOR Port_A
+
+
+direction roboDirection = S;
+
+void driveTile(){
+    Motor_Drive(LEFT_MOTOR, Motor_dir_forward, 30);
+    Motor_Drive(RIGHT_MOTOR, Motor_dir_forward, 30);
+    Delay(1000); 
+    Motor_Stop(LEFT_MOTOR, MOTOR_stop_float);
+    Motor_Stop(RIGHT_MOTOR, MOTOR_stop_float);
+    Delay(250);
+}
+
+void turnLeft(){
+    Motor_Drive(LEFT_MOTOR, Motor_dir_backward, 30);
+    Motor_Drive(RIGHT_MOTOR, Motor_dir_forward, 30);
+    Delay(100);
+    Motor_Stop(LEFT_MOTOR, MOTOR_stop_float);
+    Motor_Stop(RIGHT_MOTOR, MOTOR_stop_float);
+    roboDirection = (roboDirection + 1) % 4;
+}
+
+void turnRight(){
+    Motor_Drive(LEFT_MOTOR, Motor_dir_forward, 30);
+    Motor_Drive(RIGHT_MOTOR, Motor_dir_backward, 30);
+    Delay(100);
+    Motor_Stop(LEFT_MOTOR, MOTOR_stop_float);
+    Motor_Stop(RIGHT_MOTOR, MOTOR_stop_float);
+    roboDirection = (roboDirection - 1) % 4;
+}
+
+void turnAround(){
+    turnLeft();
+    turnLeft();
+}
---
+++ CodeInProgress/motion.h
@@ -0,0 +1,9 @@
+#ifndef MOTION_H
+#define MOTION_H
+
+void driveTile();
+void turnLeft();
+void turnRight();
+void turnAround();
+
+#endif 

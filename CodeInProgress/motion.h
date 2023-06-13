b59f73317afd514c99d4b988d6dbdce0c190f2d3
Florian <prods-cantos0w@icloud.com>
2023-06-13 15:17:05 +0200

    Refine function for drive and turn

--- CodeInProgress/motion.h
+++ CodeInProgress/motion.h
@@ -1,9 +1,14 @@
 #ifndef MOTION_H
 #define MOTION_H
 
+
+void initMotorPorts();
 void driveTile();
+void turn(uint8_t);
 void turnLeft();
 void turnRight();
 void turnAround();
+void makeBeep();
+uint32_t getAbsDiff(uint32_t, uint32_t);
 
 #endif 
